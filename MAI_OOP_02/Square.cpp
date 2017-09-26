f7dfdc7f8fb0db12094fc7d0c8f72d001acf8172
Julia Vav <julia.vav@gmail.com>
2017-09-26 22:22:22 +0300

    added: .cpp and .h for square

---
+++ MAI_OOP_02/Square.cpp
@@ -0,0 +1,58 @@
+#include "Square.h"
+#include <iostream>
+#include <cmath>
+
+Square::Square() : Square(0) {
+}
+
+Square::Square(long int i) : side_a(i) {
+	std::cout << "Square created: " << side_a << std::endl;
+}
+
+Square::Square(const Square& orig) {
+	std::cout << "Square copy created" << std::endl;
+	side_a = orig.side_a;
+}
+
+Square& Square::operator=(const Square& right) {
+
+	if (this == &right) return *this;
+
+	std::cout << "Square copied" << std::endl;
+	side_a = right.side_a;
+
+	return *this;
+}
+
+Square& Square::operator++() {
+
+	side_a++;
+
+	return *this;
+}
+
+Square operator+(const Square& left, const Square& right) {
+
+	return Square(left.side_a + right.side_a);
+}
+double Square::SquareF() {
+
+	return side_a*side_a;
+}
+
+Square::~Square() {
+	std::cout << "Square deleted" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& os, const Square& obj) {
+
+	os << "a=" << obj.side_a << std::endl;
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, Square& obj) {
+
+	is >> obj.side_a;
+
+	return is;
+}
--- MAI_OOP_02/Square.h
+++ MAI_OOP_02/Square.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef SQUARE_H
+#define	SQUARE_H
+#include <cstdlib>
+#include <iostream>
+
+class Square {
+public:
+	Square();
+	Square(const Square& orig);
+	Square(long int i);
+
+	Square& operator++();
+	double SquareF();
+	friend Square operator+(const Square& left, const Square& right);
+
+	friend std::ostream& operator<<(std::ostream& os, const Square& obj);
+	friend std::istream& operator>>(std::istream& is, Square& obj);
+
+	Square& operator=(const Square & right);
+
+	virtual ~Square();
+private:
+	long int side_a;
+};
+
+#endif	/* SQUARE_H */
