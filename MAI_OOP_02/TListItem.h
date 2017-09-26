db85162dfa1c3daff8128ce5108bb4d39f141d8d
Julia Vav <julia.vav@gmail.com>
2017-09-26 22:39:50 +0300

    added: .cpp and .h for list element

--- MAI_OOP_02/TListItem.h
+++ MAI_OOP_02/TListItem.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef TLISTITEM_H
+#define	TLISTITEM_H
+
+#include "Square.h"
+class TListItem {
+public:
+	TListItem(const Square& triangle);
+	TListItem(const TListItem& orig);//copy constr
+	friend std::ostream& operator<<(std::ostream& os, const TListItem& obj);
+
+	TListItem* SetNext(TListItem* next);
+	TListItem* GetNext();
+	Square GetSquare() const;
+
+	virtual ~TListItem();
+private:
+	Square square;
+	TListItem *next;
+};
+
+#endif	/* TLISTITEM_H */
